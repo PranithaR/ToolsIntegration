5d04fad0332ee3752aa4a09fc13cc49b4f63d1a4
PranithaR <pranitha.reddy28@gmail.com>
2017-04-23 11:23:34 -0400

    Create variablessettings.sh

---
+++ variablessettings.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
+export PATH=$PATH:$JAVA_HOME/bin
+export M2_HOME=/Users/pranu/Desktop/dockerFiles/fig/apache-maven-3.5.0
+export PATH=$PATH:$M2_HOME/bin
